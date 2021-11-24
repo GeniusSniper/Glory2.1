f5ff0707fa3a2fd11a84836a9ed3293e9f50decb
GeniusSniper <tiwei61342@gmail.com>
2021-11-24 11:43:41 -0600

    reset the dev and pro

--- webpack.dev.js
+++ webpack.dev.js
@@ -14,30 +14,32 @@ const infoColor = (_message) => {
 module.exports = merge(commonConfiguration, {
   mode: "development",
   devtool: "eval-source-map",
-  plugins: [new MiniCSSExtractPlugin()],
-//   devServer: {
-//     host: "0.0.0.0",
-//     port: portFinderSync.getPort(8080),
-//     contentBase: "./dist",
-//     watchContentBase: true,
-//     open: true,
-//     https: false,
-//     useLocalIp: true,
-//     disableHostCheck: true,
-//     overlay: true,
-//     noInfo: true,
-//     after: function (app, server, compiler) {
-//       const port = server.options.port;
-//       const https = server.options.https ? "s" : "";
-//       const localIp = ip.v4.sync();
-//       const domain1 = `http${https}://${localIp}:${port}`;
-//       const domain2 = `http${https}://localhost:${port}`;
+  plugins: [
+    new MiniCSSExtractPlugin(),
+  ],
+  devServer: {
+    host: "0.0.0.0",
+    port: portFinderSync.getPort(8080),
+    // contentBase: "./dist",
+    watchContentBase: true,
+    open: true,
+    https: false,
+    useLocalIp: true,
+    disableHostCheck: true,
+    overlay: true,
+    noInfo: true,
+    after: function (app, server, compiler) {
+      const port = server.options.port;
+      const https = server.options.https ? "s" : "";
+      const localIp = ip.v4.sync();
+      const domain1 = `http${https}://${localIp}:${port}`;
+      const domain2 = `http${https}://localhost:${port}`;
 
-//       console.log(
-//         `Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(
-//           domain2
-//         )}`
-//       );
-//     },
-//   },
+      console.log(
+        `Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(
+          domain2
+        )}`
+      );
+    },
+  },
 });
