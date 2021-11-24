716bd2fbc9c79f399763985fc05decdb079d78e9
GeniusSniper <tiwei61342@gmail.com>
2021-11-24 11:28:41 -0600

    changes in dev

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,9 @@
-const { merge } = require('webpack-merge')
-const commonConfiguration = require('./webpack.common.js')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { merge } = require("webpack-merge");
+const commonConfiguration = require("./webpack.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = merge(
-    commonConfiguration,
-    {
-        mode: 'production',
-        plugins: [new MiniCssExtractPlugin()],
-        devtool: 'eval-source-map'
-    }
-)
+module.exports = merge(commonConfiguration, {
+  mode: "production",
+  plugins: [new MiniCssExtractPlugin()],
+  devtool: "source-map",
+});
